2dfede0fe64f4237f2f1410ea7db09091af7af14
Svenkia <contrinostefano4@gmail.com>
2026-09-22 16:03:15 +0200

    Added the leaf falling down the Consecrated Tree when it is struck

--- Gores/Trees/ConsecratedLeaf.cs
+++ Gores/Trees/ConsecratedLeaf.cs
@@ -10,24 +10,17 @@ namespace TheLawOfTheGods.Gores.Trees
     {
         public override void SetStaticDefaults()
         {
-            // Imposta la foglia come sicura per la modalità "Child Safety" di Terraria
             ChildSafety.SafeGore[Type] = true;
         }
 
         public override void OnSpawn(Gore gore, IEntitySource source)
         {
             ChildSafety.SafeGore[gore.type] = true;
-
-            // Dà alla foglia una velocità e una direzione casuali quando appare
             gore.velocity = new Vector2(Main.rand.NextFloat() - 0.5f, Main.rand.NextFloat() * MathHelper.TwoPi);
-
-            // Se lo sprite sheet ha 8 frame per la rotazione/fluttuazione della foglia
             gore.numFrames = 8;
             gore.frame = (byte)Main.rand.Next(8);
             gore.frameCounter = (byte)Main.rand.Next(8);
-
-            // Copia la fisica di movimento delle foglie d'albero vanilla (ID 910 = foglia d'albero standard)
-            UpdateType = 910;
+            UpdateType = 910; // Riferimento al comportamento di una foglia vanilla di Terraria
         }
     }
 }
--- Tiles/ConsecratedLand/ConsecratedTree.cs
+++ Tiles/ConsecratedLand/ConsecratedTree.cs
@@ -1,10 +1,11 @@
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
-using TheLawOfTheGods.Tiles.ConsecratedLand; // Assicurati che i namespace siano corretti
 
 namespace TheLawOfTheGods.Tiles.ConsecratedLand
 {
@@ -41,9 +42,79 @@ namespace TheLawOfTheGods.Tiles.ConsecratedLand
         }
 
         public override void SetTreeFoliageSettings(int i, int j, Tile tile, int xoffset, ref int treeFrame, int floorY, ref int topTextureFrameWidth, ref int topTextureFrameHeight) {
-			// This is where fancy code could go, but let's save that for an advanced example
+			
 		}
+
+        public override int TreeLeaf() => ModContent.GoreType<Gores.Trees.ConsecratedLeaf>();
         public override int DropWood() => ItemID.Wood;
+
+        public override bool Shake(int x, int y, ref bool createLeaves)
+        {
+            int randAmt = Main.rand.Next(1, 3);
+            
+            if (Main.getGoodWorld && Main.rand.NextBool(15))
+            {
+                Projectile.NewProjectile(new EntitySource_ShakeTree(x, y), x * 16, y * 16, Main.rand.NextFloat(-100f, 100f) * 0.002f, 0f, ProjectileID.Bomb, 0, 0f, Player.FindClosest(new Vector2(x * 16, y * 16), 16, 16));
+            }
+            else if (Main.rand.NextBool(7))
+            {
+                createLeaves = true;
+                Item.NewItem(WorldGen.GetItemSource_FromTreeShake(x, y), x * 16, y * 16, 16, 16, ItemID.Acorn, randAmt);
+            }
+            else if (Main.rand.NextBool(35) && Main.halloween)
+            {
+                createLeaves = true;
+                Item.NewItem(WorldGen.GetItemSource_FromTreeShake(x, y), x * 16, y * 16, 16, 16, ItemID.RottenEgg, randAmt);
+            }
+            else if (Main.rand.NextBool(12))
+            {
+                createLeaves = true;
+                // Sostituisci DropWood() con il legno vanilla che preferisci, es. ItemID.Wood
+                Item.NewItem(WorldGen.GetItemSource_FromTreeShake(x, y), x * 16, y * 16, 16, 16, ItemID.Wood, Main.rand.Next(1, 4));
+            }
+            else if (Main.rand.NextBool(20))
+            {
+                createLeaves = true;
+                int coin = ItemID.CopperCoin;
+                int amount = Main.rand.Next(50, 100);
+                if (Main.rand.NextBool(30))
+                {
+                    coin = ItemID.GoldCoin;
+                    amount = 1;
+                    if (Main.rand.NextBool(5))
+                        amount++;
+
+                    if (Main.rand.NextBool(10))
+                        amount++;
+                }
+                else if (Main.rand.NextBool(10))
+                {
+                    coin = ItemID.SilverCoin;
+                    amount = Main.rand.Next(1, 21);
+                    if (Main.rand.NextBool(3))
+                        amount += Main.rand.Next(1, 21);
+
+                    if (Main.rand.NextBool(4))
+                        amount += Main.rand.Next(1, 21);
+                }
+
+                Item.NewItem(WorldGen.GetItemSource_FromTreeShake(x, y), x * 16, y * 16, 16, 16, coin, amount);
+            }
+            else if (Main.rand.NextBool(20))
+            {
+                createLeaves = true;
+                int type = NPCID.EnchantedNightcrawler; // Creatura vanilla
+                NPC.NewNPC(new EntitySource_ShakeTree(x, y), x * 16, y * 16, type);
+            }
+            else if (Main.rand.NextBool(15))
+            {
+                createLeaves = true;
+                int type = ItemID.FallenStar;
+                Item.NewItem(WorldGen.GetItemSource_FromTreeShake(x, y), new Vector2(x, y) * 16, type, randAmt);
+            }
+    
+            return false;
+        }
 
         // Restituiscono le texture salvate in precedenza
         public override Asset<Texture2D> GetTexture() => texture;
