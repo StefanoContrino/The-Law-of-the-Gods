98abe4914906a20c98df34ea3db4deb060559429
Svenkia <contrinostefano4@gmail.com>
2026-09-22 08:18:45 +0200

    .

--- Tiles/ConsecratedLand/ConsecratedTreeSapling.cs
+++ Tiles/ConsecratedLand/ConsecratedTreeSapling.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.Enums;
+using Terraria.GameContent.Metadata; // Fondamentale per TileMaterials
 using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
@@ -22,6 +23,9 @@ namespace TheLawOfTheGods.Tiles.ConsecratedLand
             TileID.Sets.TreeSapling[Type] = true;
             TileID.Sets.SwaysInWindBasic[Type] = true;
 
+            // CRUCIALE: Dice al gioco che questo tile si comporta come una pianta (interagisce con il fertilizzante)
+            TileMaterials.SetForTileId(Type, TileMaterials._materialsByName["Plant"]);
+
             TileObjectData.newTile.Width = 1;
             TileObjectData.newTile.Height = 2;
             TileObjectData.newTile.Origin = new Point16(0, 1);
@@ -34,13 +38,12 @@ namespace TheLawOfTheGods.Tiles.ConsecratedLand
             TileObjectData.newTile.StyleHorizontal = true;
             TileObjectData.newTile.DrawFlipHorizontal = true;
             TileObjectData.newTile.RandomStyleRange = 3;
-            TileObjectData.newTile.StyleMultiplier = 3; // Fondamentale se hai più stili di sapling/albero
+            TileObjectData.newTile.StyleMultiplier = 3; 
             TileObjectData.newTile.WaterPlacement = LiquidPlacement.NotAllowed;
             TileObjectData.newTile.LavaDeath = true;
 
             TileObjectData.addTile(Type);
 
-            // Usa MapObject.Sapling per sicurezza o la tua chiave hjson
             AddMapEntry(new Color(100, 200, 100), Language.GetText("MapObject.Sapling"));
             
             AdjTiles = [TileID.Saplings];
@@ -48,7 +51,6 @@ namespace TheLawOfTheGods.Tiles.ConsecratedLand
 
         public override void SetSpriteEffects(int i, int j, ref SpriteEffects effects)
         {
-            // Modificato leggermente per seguire lo standard di alternanza dell'ExampleMod
             if (i % 2 == 0)
             {
                 effects = SpriteEffects.FlipHorizontally;
@@ -57,13 +59,11 @@ namespace TheLawOfTheGods.Tiles.ConsecratedLand
 
         public override void RandomUpdate(int i, int j)
         {
-            // Controllo casuale per la crescita (1 volta su 20 tick di aggiornamento casuale)
             if (!WorldGen.genRand.NextBool(20))
             {
                 return;
             }
 
-            // Metodo pulito e sicuro preso dall'ExampleMod
             bool growSuccess = WorldGen.GrowTree(i, j);
             bool isPlayerNear = WorldGen.PlayerLOS(i, j);
 
