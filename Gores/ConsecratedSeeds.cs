bdcf2243018d38ed11fa633f8be278be8da32022
Svenkia <contrinostefano4@gmail.com>
2026-09-22 09:34:21 +0200

    Understand that the problem isn't the code or the sprites, but how Terraria Manages fertilizer use and tree growth

---
+++ Gores/ConsecratedSeeds.cs
@@ -0,0 +1,78 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TheLawOfTheGods.Tiles.ConsecratedLand;
+
+namespace TheLawOfTheGods.Items
+{
+    public class ConsecratedSeeds : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            // Quanti pezzi servono per sbloccarlo nella Journey Mode (25 è lo standard per i semi)
+            Item.ResearchUnlockCount = 25;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 14;
+            Item.height = 14;
+            Item.maxStack = Item.CommonMaxStack; // Fino a 9999 pezzi
+            
+            // Impostazioni d'uso (simile ai semi vanilla)
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
+            
+            Item.rare = ItemRarityID.White;
+            Item.value = Item.buyPrice(copper: 20); // Prezzo di vendita/acquisto base
+        }
+
+        // Logica per trasformare la terra in ConsecratedGrass quando si clicca
+        public override bool? UseItem(Player player)
+        {
+            // Coordinate del tile su cui il giocatore sta cliccando col mouse
+            int i = Player.tileTargetX;
+            int j = Player.tileTargetY;
+
+            Tile tile = Main.tile[i, j];
+
+            // Verifica se il blocco cliccato è attivo ed è Terra Vanilla (Dirt)
+            if (tile.HasTile && tile.TileType == TileID.Dirt)
+            {
+                // Trasforma il blocco in ConsecratedGrass
+                tile.TileType = (ushort)ModContent.TileType<ConsecratedGrass>();
+                
+                // Suono del posizionamento del seme e aggiornamento grafico dei blocchi adiacenti
+                SoundEngine.PlaySound(SoundID.Dig, new Vector2(i * 16, j * 16));
+                WorldGen.SquareTileFrame(i, j, true);
+
+                // In multiplayer, invia il pacchetto di rete per sincronizzare il cambio blocco con gli altri giocatori
+                if (Main.netMode == NetmodeID.MultiplayerClient)
+                {
+                    NetMessage.SendTileSquare(-1, i, j, 1);
+                }
+
+                return true; // Consuma l'item
+            }
+
+            return false; // Non consuma l'item se non clicchi sulla terra
+        }
+
+        /*
+        // Se vuoi aggiungere una ricetta per sintetizzare i semi (es. da una ghianda o da un fiore)
+        public override void AddRecipes()
+        {
+            // CreateRecipe(5)
+            //     .AddIngredient(ItemID.Acorn)
+            //     .AddTile(TileID.WorkBenches)
+            //     .Register();
+        }
+        */
+    }
+}
---
+++ Tiles/ConsecratedLand/ConsecratedTreeSapling.cs
@@ -0,0 +1,90 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.GameContent.Metadata; // Fondamentale per TileMaterials
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace TheLawOfTheGods.Tiles.ConsecratedLand
+{
+    public class ConsecratedTreeSapling : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+
+            TileID.Sets.CommonSapling[Type] = true;
+            TileID.Sets.TreeSapling[Type] = true;
+            TileID.Sets.SwaysInWindBasic[Type] = true;
+
+            // CRUCIALE: Dice al gioco che questo tile si comporta come una pianta (interagisce con il fertilizzante)
+            TileMaterials.SetForTileId(Type, TileMaterials._materialsByName["Plant"]);
+
+            TileObjectData.newTile.UsesCustomCanPlace = true;
+            TileObjectData.newTile.Width = 1;
+            TileObjectData.newTile.Height = 2;
+            TileObjectData.newTile.Origin = new Point16(0, 1);
+            TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile, TileObjectData.newTile.Width, 0);
+            TileObjectData.newTile.AnchorValidTiles = [ModContent.TileType<ConsecratedGrass>(), TileID.Grass];
+
+            TileObjectData.newTile.CoordinateWidth = 16;
+            TileObjectData.newTile.CoordinateHeights = [16, 18];
+            TileObjectData.newTile.CoordinatePadding = 2;
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.DrawFlipHorizontal = true;
+            TileObjectData.newTile.RandomStyleRange = 3;
+            TileObjectData.newTile.StyleMultiplier = 3; 
+            TileObjectData.newTile.WaterPlacement = LiquidPlacement.NotAllowed;
+            TileObjectData.newTile.LavaDeath = true;
+
+            TileObjectData.addTile(Type);
+
+            AddMapEntry(new Color(100, 200, 100), Language.GetText("MapObject.Sapling"));
+            
+            AdjTiles = [TileID.Saplings];
+        }
+
+        public override void SetSpriteEffects(int i, int j, ref SpriteEffects effects)
+        {
+            if (i % 2 == 0)
+            {
+                effects = SpriteEffects.FlipHorizontally;
+            }
+        }
+
+        public override void RandomUpdate(int i, int j)
+        {
+           if (!WorldGen.genRand.NextBool(20)) {
+				return;
+			}
+
+			Tile tile = Framing.GetTileSafely(i, j); // Safely get the tile at the given coordinates
+			bool growSuccess; // A bool to see if the tree growing was successful.
+
+			// Style 0 is for the ExampleTree sapling, and style 1 is for ExamplePalmTree, so here we check frameX to call the correct method.
+			// Any pixels before 54 on the tilesheet are for ExampleTree while any pixels above it are for ExamplePalmTree
+			if (tile.TileFrameX < 54) {
+				growSuccess = WorldGen.GrowTree(i, j);
+			}
+			else {
+				growSuccess = WorldGen.GrowPalmTree(i, j);
+			}
+
+			// A flag to check if a player is near the sapling
+			bool isPlayerNear = WorldGen.PlayerLOS(i, j);
+
+			// If growing the tree was a success and the player is near, show growing effects
+			if (growSuccess && isPlayerNear) {
+				WorldGen.TreeGrowFXCheck(i, j);
+			}
+		}
+
+        public override void NumDust(int i, int j, bool fail, ref int num) => num = fail ? 1 : 3;
+    }
+}
